c38909c6a0edfd7eaa5876b242d7b3364974c596
Sean Kennealy <spkennealy@gmail.com>
2019-08-02 09:45:42 -0700

    Add iteration exercise #1

--- Udemy_C#_basics_excercises/Control_Flow/Control_Flow/Program.cs
+++ Udemy_C#_basics_excercises/Control_Flow/Control_Flow/Program.cs
@@ -6,10 +6,15 @@ namespace Control_Flow
     {
         public static void Main(string[] args)
         {
-            Conditionals.Excercise1();
-            Conditionals.Excercise2();
-            Conditionals.Excercise3();
-            Conditionals.Excercise4();
+            //Conditionals.Excercise1();
+            //Conditionals.Excercise2();
+            //Conditionals.Excercise3();
+            //Conditionals.Excercise4();
+            //IterationStatements.Excercise1();
+            IterationStatements.Excercise2();
+            //IterationStatements.Excercise3();
+            //IterationStatements.Excercise4();
+            //IterationStatements.Excercise5();
         }
 
         // 1- Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number,
@@ -81,9 +86,62 @@ namespace Control_Flow
                 var demerits = diff / 5;
                 if (demerits >= 12)
                     Console.WriteLine("License Suspended!");
-                else 
+                else
                     Console.WriteLine("You have received " + demerits + " demerits.");
             }
         }
-    } 
+    }
+
+
+    public class IterationStatements
+    {
+
+        // 1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on
+        // the console.
+
+        public static void Excercise1()
+        {
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count += 1;
+            }
+
+            Console.WriteLine("Count of numbers divisible by 3: " + count);
+        }
+
+        // 2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the
+        // previously entered numbers and display it on the console.
+
+        public static void Excercise2()
+        {
+
+        }
+
+        // 3- Write a program and ask the user to enter a number.Compute the factorial of the number and print it on the console.
+        // For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+        public static void Excercise3()
+        {
+
+        }
+
+        // 4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number.If the
+        // user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving
+        // correctly, you can display the secret number on the console first.)
+
+        public static void Excercise4()
+        {
+
+        }
+
+        // 5- Write a program and ask the user to enter a series of numbers separated by comma.Find the maximum of the numbers
+        // and display it on the console.For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+        public static void Excercise5()
+        {
+
+        }
+    }
 }
