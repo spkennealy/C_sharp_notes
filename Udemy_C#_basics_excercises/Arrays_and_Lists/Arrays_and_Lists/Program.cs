bd0093b439514a944bec51d81edbca4db9cda5dc
Sean Kennealy <spkennealy@gmail.com>
2019-08-08 20:19:50 -0700

    Arrays & Lists exercise 1

--- Udemy_C#_basics_excercises/Arrays_and_Lists/Arrays_and_Lists/Program.cs
+++ Udemy_C#_basics_excercises/Arrays_and_Lists/Arrays_and_Lists/Program.cs
@@ -1,34 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Arrays_and_Lists
 {
-    class MainClass
+    class ArraysAndLists
     {
         public static void Main(string[] args)
         {
-            // 1 - When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
-
-            // If no one likes your post, it doesn't display anything.
-            // If only one person likes your post, it displays: [Friend's Name] likes your post.
-            // If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
-            // If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like your post.
-            // Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
-
-
-
-            // 2- Write a program and ask the user to enter their name.Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.
-
-
-
-            // 3- Write a program and ask the user to enter 5 numbers.If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
-
-
-
-            // 4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates.Display the unique numbers that the user has entered.
-
-
-
-            // 5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
+            ArraysAndLists.Exercise1();
+            //ArraysAndLists.Exercise2();
+            //ArraysAndLists.Exercise3();
+            //ArraysAndLists.Exercise4();
+            //ArraysAndLists.Exercise5();
         }
+
+        // 1 - When you post a message on Facebook, depending on the number of people who like your post, Facebook displays
+        // different information.
+
+        // If no one likes your post, it doesn't display anything.
+        // If only one person likes your post, it displays: [Friend's Name] likes your post.
+        // If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+        // If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like
+        // your post.
+
+        // Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying
+        // a name). Depending on the number of names provided, display a message based on the above pattern.
+
+        public static void Exercise1()
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a name or press enter to end: ");
+                var name = Console.ReadLine();
+
+                if (name.Length == 0)
+                {
+                    break;
+                }
+
+                names.Add(name);
+
+                if (names.Count == 1)
+                {
+                    Console.WriteLine("Names: " + names[0]);
+                }
+                else if (names.Count == 2)
+                {
+                    Console.WriteLine("Names: " + names[0] + ", " + names[1]);
+                }
+                else if (names.Count > 2)
+                {
+                    Console.WriteLine("Names: " + names[0] + ", " + names[1] + ", and " + (names.Count - 2) + "others");
+                }
+            }
+        }
+
+        // 2- Write a program and ask the user to enter their name.Use an array to reverse the name and then store the result in
+        // a new string. Display the reversed name on the console.
+
+        //public static void Exercise2()
+        //{
+
+        //}
+
+        // 3- Write a program and ask the user to enter 5 numbers.If a number has been previously entered, display an error message
+        // and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the
+        // console.
+
+        //public static void Exercise3()
+        //{
+
+        //}
+
+        // 4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may
+        // include duplicates.Display the unique numbers that the user has entered.
+
+        //public static void Exercise4()
+        //{
+
+        //}
+
+        // 5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is
+        // empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3
+        //  smallest numbers in the list.
+
+        //public static void Exercise5()
+        //{
+
+        //}
     }
 }
