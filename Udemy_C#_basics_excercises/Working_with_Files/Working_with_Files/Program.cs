32da45c8cb7ff11e6c9650592565daf40e16bbf2
Sean Kennealy <spkennealy@gmail.com>
2019-08-17 10:23:56 -0700

    Section 9 execercise 2

--- Udemy_C#_basics_excercises/Working_with_Files/Working_with_Files/Program.cs
+++ Udemy_C#_basics_excercises/Working_with_Files/Working_with_Files/Program.cs
@@ -8,7 +8,7 @@ namespace Working_with_Files
         public static void Main(string[] args)
         {
             Files.Exercise1();
-            //Files.Exercise2();
+            Files.Exercise2();
         }
 
         // 1- Write a program that reads a text file and displays the number of words.
@@ -16,17 +16,25 @@ namespace Working_with_Files
         public static void Exercise1()
         {
             var text = File.ReadAllText(@"/Users/seankennealy/Desktop/Insightly/C_sharp_notes/Udemy_C#_basics_excercises/Working_with_Files/Working_with_Files/GSW.txt");
-            var textWithoutNewLines = text.Replace(System.Environment.NewLine, "");
-            Console.WriteLine(textWithoutNewLines);
-            var wordCount = textWithoutNewLines.Split(' ').Length;
+            var wordCount = text.Split(' ').Length;
             Console.WriteLine("Total words: " + wordCount);
         }
 
         // 2- Write a program that reads a text file and displays the longest word in the file.
 
-        //public static void Exercise2()
-        //{
+        public static void Exercise2()
+        {
+            var text = File.ReadAllText(@"/Users/seankennealy/Desktop/Insightly/C_sharp_notes/Udemy_C#_basics_excercises/Working_with_Files/Working_with_Files/GSW.txt");
+            var words = text.Split(' ');
+            var longestWord = "";
 
-        //}
+            foreach (var word in words)
+            {
+                if (word.Length > longestWord.Length)
+                    longestWord = word;
+            }
+
+            Console.WriteLine("The longest word is: " + longestWord);
+        }
     }
 }
