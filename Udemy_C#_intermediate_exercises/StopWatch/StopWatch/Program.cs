b2bb5e24391a2d03ef910bb910b2bdb9578d3dfc
Sean Kennealy <spkennealy@gmail.com>
2019-08-25 14:34:30 -0700

    Begin class exercises

---
+++ Udemy_C#_intermediate_exercises/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+        }
+    }
+}
