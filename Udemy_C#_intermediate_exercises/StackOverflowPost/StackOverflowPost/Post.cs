c97379aabab4a2366e612032f4704a1b451e7c5f
Sean Kennealy <spkennealy@gmail.com>
2019-08-27 20:33:10 -0700

    Stackoverflow post constructor

---
+++ Udemy_C#_intermediate_exercises/StackOverflowPost/StackOverflowPost/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        // Design a class called Post. This class models a StackOverflow post. It should have properties for title,
+        // description and the date/time it was created. We should be able to up-vote or down-vote a post. We should
+        // also be able to see the current vote value. In the main method, create a post, up-vote and down-vote it a
+        // few times and then display the the current vote value.
+
+        private string Title;
+        private string Description;
+        private DateTime DateCreated;
+
+        public Post(string title, string description, DateTime currentTime)
+        {
+            Title = title;
+            Description = description;
+            DateCreated = currentTime;
+        }
+
+        public void Upvote()
+        {
+
+        }
+    }
+}
---
+++ Udemy_C#_intermediate_exercises/StackOverflowPost/StackOverflowPost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
