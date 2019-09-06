feca97caa48c5f5086bde945f9ef5a3b5f53215a
Sean Kennealy <spkennealy@gmail.com>
2019-09-06 15:30:28 +0200

    Add beginning of DbCommand class

---
+++ Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/DbCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Design_A_Database_Connection
+{
+    public class DbCommand
+    {
+        private DbConnection DbConnection;
+
+        public DbCommand(DbConnection dbConnection)
+        {
+            if (dbConnection is null)
+                throw new InvalidOperationException();
+
+            DbConnection = dbConnection;
+        }
+    }
+}
