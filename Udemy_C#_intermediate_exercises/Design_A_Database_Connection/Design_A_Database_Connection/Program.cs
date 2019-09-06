11d5a8b42db07ae59e672fd43c1f103d04fb1912
Sean Kennealy <spkennealy@gmail.com>
2019-09-06 15:02:30 +0200

    Working db connection classes completed

--- Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/Program.cs
+++ Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/Program.cs
@@ -6,7 +6,13 @@ namespace Design_A_Database_Connection
     {
         public static void Main(string[] args)
         {
-            
+            var sqlConnection = new SqlConnection("alk2lf");
+            sqlConnection.OpenConnection();
+            sqlConnection.CloseConnection();
+
+            var oracleConnection = new OracleConnection("jkwuek");
+            oracleConnection.OpenConnection();
+            oracleConnection.CloseConnection();
         }
     }
 }
--- Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/SqlConnection.cs
+++ Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/SqlConnection.cs
@@ -20,23 +20,4 @@ namespace Design_A_Database_Connection
             Console.WriteLine("Closing the SQL connection.");
         }
     }
-
-    public class OracleConnection : DbConnection
-    {
-        public OracleConnection(string connectionString)
-            : base(connectionString)
-        {
-
-        }
-
-        public override void OpenConnection()
-        {
-            Console.WriteLine("Opening the Oracle connection.");
-        }
-
-        public override void CloseConnection()
-        {
-            Console.WriteLine("Closing the Oracle connection.");
-        }
-    }
 }
