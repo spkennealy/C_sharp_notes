5ab9992d2cc642ce3b3da8674756a63905faea42
Sean Kennealy <spkennealy@gmail.com>
2019-09-12 09:45:51 +0200

    Complete Polymorphism exercises

--- Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/Program.cs
+++ Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/Program.cs
@@ -7,12 +7,18 @@ namespace Design_A_Database_Connection
         public static void Main(string[] args)
         {
             var sqlConnection = new SqlConnection("alk2lf");
-            sqlConnection.OpenConnection();
-            sqlConnection.CloseConnection();
+            //sqlConnection.OpenConnection();
+            //sqlConnection.CloseConnection();
+            var sqlInstructions = "SQL instructions are here!";
+            var dbCommandSQL = new DbCommand(sqlConnection, sqlInstructions);
+            dbCommandSQL.Execute();
 
             var oracleConnection = new OracleConnection("jkwuek");
-            oracleConnection.OpenConnection();
-            oracleConnection.CloseConnection();
+            //oracleConnection.OpenConnection();
+            //oracleConnection.CloseConnection();
+            var oracleInstructions = "Oracle instructions are here!";
+            var dbCommandOracle = new DbCommand(oracleConnection, oracleInstructions);
+            dbCommandOracle.Execute();
         }
     }
 }
