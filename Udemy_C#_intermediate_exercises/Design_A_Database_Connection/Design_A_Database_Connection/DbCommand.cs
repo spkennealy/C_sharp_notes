f6400b5dd37aa06de2c58b0b3934acaa5cded0b5
Sean Kennealy <spkennealy@gmail.com>
2019-09-12 09:39:56 +0200

    Add constructor for DbCommand

--- Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/DbCommand.cs
+++ Udemy_C#_intermediate_exercises/Design_A_Database_Connection/Design_A_Database_Connection/DbCommand.cs
@@ -4,14 +4,18 @@ namespace Design_A_Database_Connection
 {
     public class DbCommand
     {
-        private DbConnection DbConnection;
+        private readonly DbConnection _dbConnection;
+        private readonly string _instructions;
 
-        public DbCommand(DbConnection dbConnection)
+        public DbCommand(DbConnection dbConnection, string instructions)
         {
             if (dbConnection is null)
-                throw new InvalidOperationException();
+                throw new ArgumentException("There must be a DbConnection to run a command");
+            if (String.IsNullOrWhiteSpace(instructions))
+                throw new ArgumentException("There must be instructions provided with the DbConnection");
 
-            DbConnection = dbConnection;
+            _dbConnection = dbConnection;
+            _instructions = instructions;
         }
     }
 }
