61df9fa6e3bca33d9f2a4454351a9eb3969eeb10
Sean Kennealy <spkennealy@gmail.com>
2019-10-26 08:36:33 -0700

    Add singleton coding exercise solution

---
+++ Udemy_Design_Patterns_in_C#_and_.NET/Exercises/Singleton_Coding_Exercise/Singleton_Coding_Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton_Coding_Exercise
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var results1 = func();
+            var results2 = func();
+            return results1 == results2;
+        }
+    }
+}
